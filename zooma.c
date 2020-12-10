c25f22986fc060fd3e9234f2605ab23308272ab9
AlmightyCrickityCrick <53330933+AlmightyCrickityCrick@users.noreply.github.com>
2020-12-09 16:10:25 -0800

    Balls movement added

--- zooma.c
+++ zooma.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <windows.h>
+#include <stdlib.h>
+#include <time.h>
 int gameOver; //variabila care determina daca jocul inca mai mergw
 char board[15][30]= {
     " #############################",
@@ -24,12 +26,14 @@ const int hx = 0;   //coordonata x a cursorului vertical
 const int wy = 14; // coordonata y a cursorului orizontal
 int cx, cy, hy, wx, score; // c - canon, h - cursor vertical, w - cursor orizontal.
 char can; //caracterul din canon
-int tailX[100] = {};
-int tailY[100] = {}; //coordonatele mingilor
-char tailC[100] = {}; //caracterul mingilor
-int nTail; //numarul de mingi
-enum Direction {S = 0, L, R, U, D}; //directiile posibile de deplasare
+int tailX[41];
+int tailY[41]; //coordonatele mingilor
+char tailC[41]; //caracterul mingilor
+int nTail = 41; //numarul de mingi
+int currentTail = 0;
+enum Direction {S = 0, L, R, U, D, E}; //directiile posibile de deplasare acursoarelor
 enum Direction dir = S;
+clock_t init, fin; //timpul pentru cronometrarea aparitiei mingilor
 
 void cls() //in schimbul functiei de curatare ca aia provoaca epilepsie.
 {
@@ -42,11 +46,56 @@ void cls() //in schimbul functiei de curatare ca aia provoaca epilepsie.
     Position.Y = 0;
     SetConsoleCursorPosition(hOut, Position);
 }
-void Timer()
+void LevelAssignBall() //Stabileste Ruta mingilor pentru nivelul anumit.
 {
+    int i = 10, j = 3;
+
+    for (int k = 0; k < nTail; k++)
+    {
+        if (i > 2 && j == 3)
+        {
+            tailX[k] = j;
+            tailY[k] = i;
+            i--;
+        }
+        else if(j < 27)
+        {
+            tailX[k] = j;
+            tailY[k] = i;
+            j++;
+        }
+        else
+        {
+            tailX[k] = j;
+            tailY[k] = i;
+            i++;
+        }
+        tailC[k] = '*';
+    }
 
 }
-void Setup()
+
+
+char AddBall() //Functie care adauga mingi la sfarsitul cozii
+{
+    int b = rand() % 4;
+    switch(b)
+    {
+        case 0 : return 'B'; break;
+        case 1 : return 'R'; break;
+        case 2 : return 'G'; break;
+        case 3: return 'W'; break;
+    }
+}
+void BallMove(int x) //Functie responsabila de miscarea cozii.
+{
+    for (int i = 1; i < x; i++)
+        tailC[i - 1] = tailC[i];
+    if (currentTail >= nTail) tailC[x - 1] = '*';
+    else tailC[x - 1] = AddBall();
+    init = clock();
+}
+void Setup() //Seteaza parametrii la inceputul jocului.
 {
 
     gameOver = 0;
@@ -57,14 +106,12 @@ void Setup()
 
     hy = 2;
     wx = 2;
-   // nTail = 41;
-    //tailX = {};
-    //tailY = {};
-    //tailC = {'*'};
+
+    LevelAssignBall();
 
 }
 
-void Draw()
+void Draw() //Deseneaza board-ul
 {
     cls();
     for (int i = 0; i < HEIGHT; i++)
@@ -87,10 +134,9 @@ void Draw()
         printf("\n");
     }
 }
-
-void Input()
+void Input() //Verifica input-ul
 {
-    char in = 'A';
+    char in = 'A'; //A este o valoare aleatorie utilizata pentru initializarea curata a variabilei in.
     if(_kbhit()) in = _getch();
     switch(in)
     {
@@ -98,15 +144,11 @@ void Input()
         case 'a' : dir = L; break;
         case 'd' : dir = R; break;
         case 's' : dir = D; break;
+        case 13 : dir = E; break;
         default  : dir = S; break;
     }
 }
-void BallMove()
-{
-
-}
-
-void Logic()
+void Logic() //Functie ce raspunde de logica jocului.
 {
 
     switch(dir)
@@ -128,20 +170,22 @@ void Logic()
         wx = wx;
     default: break;
     }
-    if (dir == S);
-    else
-    {
-    BallMove();
-    Timer();
-    }
+    if (dir == E);
+
+    fin = clock();
+    if(fin - init >= 1000) BallMove(nTail);
+
+
     if (wx <= 0) wx = WIDTH - 1; else if (wx >= WIDTH) wx = 1;
     if (hy <= 0) hy = HEIGHT - 1; else if (hy >= HEIGHT - 1) hy = 1;
 }
 int main()
 {
     Setup();
+    init = clock();
     while(!gameOver)
     {
+        srand(time(NULL));
         Draw();
         Input();
         Logic();
